237c277dd4e95923993de9b3995645596bc0877f
Melqui Eugenio <melqui.eug@gmail.com>
2020-09-17 16:57:36 -0300

    Done

--- Restful/app/src/main/java/com/melquieugenio/restful/MainActivity.kt
+++ Restful/app/src/main/java/com/melquieugenio/restful/MainActivity.kt
@@ -4,56 +4,83 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 
 class MainActivity : AppCompatActivity() {
 
     private var user: String = "MelquiEugenio"
+    private var composite = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.github.com/")
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
 
         val service: RestService = retrofit.create(RestService::class.java)
 
-        try {
-            val repos: Call<List<Repo>?> = service.listRepos(user)
+        val repos: Observable<List<Repo>?> = service.listRepos(user)
 
-            repos.enqueue(object : Callback<List<Repo>?> {
+        composite.add(
+            (repos.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::handleResults, this::handleError))
+        )
 
-                override fun onResponse(
-                    call: Call<List<Repo>?>,
-                    response: Response<List<Repo>?>
-                ) {
-                    recyclerView.apply {
-                        layoutManager = LinearLayoutManager(this@MainActivity)
-                        adapter = MyAdapter(response.body()!!, this@MainActivity)
-                    }
-                }
+        /*repos.enqueue(object : Callback<List<Repo>?> {
 
-                override fun onFailure(call: Call<List<Repo>?>, t: Throwable) {
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Something went wrong...Please try later!",
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
-            })
+            override fun onResponse(
+                call: Call<List<Repo>?>,
+                response: Response<List<Repo>?>
+            ) {
 
-        } catch (e: Exception) {
-            Toast.makeText(this, "Failure", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onFailure(call: Call<List<Repo>?>, t: Throwable) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Something went wrong...Please try later!",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        })*/
+    }
+
+    private fun handleResults(repoList: List<Repo>?) {
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = MyAdapter(repoList!!, this@MainActivity)
         }
+    }
 
+    private fun handleError(t: Throwable) {
+        Toast.makeText(
+            this, "ERROR IN FETCHING API RESPONSE. Try again",
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        composite.clear()
     }
 }
--- Restful/app/src/main/java/com/melquieugenio/restful/RestService.kt
+++ Restful/app/src/main/java/com/melquieugenio/restful/RestService.kt
@@ -1,10 +1,10 @@
 package com.melquieugenio.restful
 
-import retrofit2.Call
+import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface RestService {
     @GET("users/{user}/repos")
-    fun listRepos(@Path("user") user: String?): Call<List<Repo>?>
+    fun listRepos(@Path("user") user: String?): Observable<List<Repo>?>
 }
