23e730625fc5ea0f1cfa6230a37b98001ce402f8
Melqui Eugenio <melqui.eug@gmail.com>
2020-09-22 20:25:27 -0300

    adding RunTime Permissions + showing Sydney

--- Maps/app/src/main/java/com/melquieugenio/maps/MapsActivity.kt
+++ Maps/app/src/main/java/com/melquieugenio/maps/MapsActivity.kt
@@ -1,43 +1,89 @@
 package com.melquieugenio.maps
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_maps.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
-
-    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
+
+        if (Build.VERSION.SDK_INT < 23)
+            callMapAsync()
+        else
+            dealingWithRunTimeLocationPermission()
+    }
+
+    private fun callMapAsync() {
+        val mapFragment = map as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    // Called when map is ready, deal with it here.
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun dealingWithRunTimeLocationPermission() {
+
+        // Handling request permission responses and allocating a reference to it
+        val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    callMapAsync() // its never called IDk why
+                } else {
+                    Toast.makeText(
+                        this,
+                        "The app need the location permission to show the map.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+
+        // Troubleshooting permission
+        when {
+            ContextCompat.checkSelfPermission( // When granted
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                callMapAsync()
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) // If the user needs info
+            -> {
+                Toast.makeText(
+                    this,
+                    "Please let the app to access the location for updating the map.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            else -> { // When not granted
+                // Request permission
+                requestPermissionLauncher.launch(
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            }
+        }
     }
 }
