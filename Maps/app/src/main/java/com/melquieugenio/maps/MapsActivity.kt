04e571b04e32ac2457c7deb52f340a3cf9c687a5
Melqui Eugenio <melqui.eug@gmail.com>
2020-09-23 20:12:02 -0300

    Showing location of the device

--- Maps/app/src/main/java/com/melquieugenio/maps/MapsActivity.kt
+++ Maps/app/src/main/java/com/melquieugenio/maps/MapsActivity.kt
@@ -1,7 +1,9 @@
 package com.melquieugenio.maps
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
@@ -9,6 +11,8 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -19,14 +23,18 @@ import kotlinx.android.synthetic.main.activity_maps.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var mCurrentLocation: Location
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
 
-        if (Build.VERSION.SDK_INT < 23)
+        if (Build.VERSION.SDK_INT < 23) {
             callMapAsync()
-        else
-            dealingWithRunTimeLocationPermission()
+        } else if (isLocationPermissionGranted()) {
+            callMapAsync()
+        }
     }
 
     private fun callMapAsync() {
@@ -35,16 +43,37 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     // Called when map is ready, deal with it here.
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                if (location != null) {
+                    mCurrentLocation = location
+                    val myLtgLng = LatLng(mCurrentLocation.latitude, mCurrentLocation.longitude)
+                    val zoomLevel = 15F
+                    googleMap.addMarker(
+                        MarkerOptions().position(myLtgLng).title("I'm here.")
+                    )
+                    googleMap.moveCamera(
+                        CameraUpdateFactory.newLatLngZoom(
+                            myLtgLng,
+                            zoomLevel
+                        )
+                    )
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Please turn on your GPS and restart the app.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
     }
 
     @RequiresApi(Build.VERSION_CODES.M)
-    private fun dealingWithRunTimeLocationPermission() {
+    private fun isLocationPermissionGranted(): Boolean {
 
         // Handling request permission responses and allocating a reference to it
         val requestPermissionLauncher =
@@ -52,11 +81,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 ActivityResultContracts.RequestPermission()
             ) { isGranted: Boolean ->
                 if (isGranted) {
-                    callMapAsync() // its never called IDk why
+                    callMapAsync()
                 } else {
                     Toast.makeText(
                         this,
-                        "The app need the location permission to show the map.",
+                        "Location permission needed to update map.",
                         Toast.LENGTH_LONG
                     ).show()
                 }
@@ -68,22 +97,25 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED -> {
-                callMapAsync()
+                return true
             }
-            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) // If the user needs info
-            -> {
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)
+            -> { // true when user try to access func after denying permission
                 Toast.makeText(
                     this,
                     "Please let the app to access the location for updating the map.",
                     Toast.LENGTH_LONG
                 ).show()
+
+                return false
             }
-            else -> { // When not granted
-                // Request permission
+            else -> { // When not granted, requesting for the first time
                 requestPermissionLauncher.launch(
                     Manifest.permission.ACCESS_FINE_LOCATION
                 )
             }
         }
+
+        return false
     }
 }
