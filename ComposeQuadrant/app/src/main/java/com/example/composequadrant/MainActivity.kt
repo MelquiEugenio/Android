e4cb7c3136344906a4f67c7bd1630a96bbf1ac3a
Melquisedeque Silva <melquiapps@gmail.com>
2025-04-20 12:25:50 -0300

    Weight and Card

---
+++ ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -0,0 +1,125 @@
+package com.example.composequadrant
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeQuadrantTheme {
+                Quadrant()
+            }
+        }
+    }
+}
+
+@Composable
+fun Quadrant(modifier: Modifier = Modifier) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(top = 50.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+        ) {
+            CustomCard(
+                modifier = Modifier.weight(1f),
+                color = Color(0xFFEADDFF),
+                title = "Text composable",
+                description = "Displays text and follows the recommended Material Design guidelines."
+            )
+            CustomCard(
+                modifier = Modifier.weight(1f),
+                color = Color(0xFFD0BCFF),
+                title = "Image composable",
+                description = "Creates a composable that lays out and draws a given Painter class object."
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            CustomCard(
+                modifier = Modifier.weight(1f),
+                color = Color(0xFFB69DF8),
+                title = "Row composable",
+                description = "A layout composable that places its children in a horizontal sequence."
+            )
+            CustomCard(
+                modifier = Modifier.weight(1f),
+                color = Color(0xFFF6EDFF),
+                title = "Column composable",
+                description = "A layout composable that places its children in a vertical sequence."
+            )
+        }
+    }
+}
+
+@Composable
+fun CustomCard(
+    modifier: Modifier,
+    color: Color = Color(0xFFEADDFF),
+    title: String,
+    description: String,
+) {
+    Card(
+        modifier = modifier.fillMaxSize(),
+        colors = CardColors(
+            containerColor = color,
+            contentColor = Color.Black,
+            disabledContainerColor = Color.Gray,
+            disabledContentColor = Color.White
+        ),
+        shape = RectangleShape,
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = Modifier.padding(8.dp), text = title, fontWeight = FontWeight.Bold
+            )
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = description,
+                textAlign = TextAlign.Justify
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun QuadrantPreview() {
+    ComposeQuadrantTheme {
+        Quadrant()
+    }
+}
