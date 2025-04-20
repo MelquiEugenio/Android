1507c442209e17c58dc6345ffed647a7bf8bf43e
Melquisedeque Silva <melquiapps@gmail.com>
2025-04-20 13:00:28 -0300

    + Material Icons

--- ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -10,14 +10,20 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,7 +43,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Quadrant(modifier: Modifier = Modifier) {
+fun Quadrant() {
     Column(
         Modifier
             .fillMaxSize()
@@ -52,13 +58,15 @@ fun Quadrant(modifier: Modifier = Modifier) {
                 modifier = Modifier.weight(1f),
                 color = Color(0xFFEADDFF),
                 title = "Text composable",
-                description = "Displays text and follows the recommended Material Design guidelines."
+                description = "Displays text and follows the recommended Material Design guidelines.",
+                icon = Icons.Default.Edit
             )
             CustomCard(
                 modifier = Modifier.weight(1f),
                 color = Color(0xFFD0BCFF),
                 title = "Image composable",
-                description = "Creates a composable that lays out and draws a given Painter class object."
+                description = "Creates a composable that lays out and draws a given Painter class object.",
+                icon = Icons.Default.Face
             )
         }
         Row(
@@ -70,13 +78,15 @@ fun Quadrant(modifier: Modifier = Modifier) {
                 modifier = Modifier.weight(1f),
                 color = Color(0xFFB69DF8),
                 title = "Row composable",
-                description = "A layout composable that places its children in a horizontal sequence."
+                description = "A layout composable that places its children in a horizontal sequence.",
+                icon = Icons.Default.Done
             )
             CustomCard(
                 modifier = Modifier.weight(1f),
                 color = Color(0xFFF6EDFF),
                 title = "Column composable",
-                description = "A layout composable that places its children in a vertical sequence."
+                description = "A layout composable that places its children in a vertical sequence.",
+                icon = Icons.Default.Done
             )
         }
     }
@@ -88,6 +98,7 @@ fun CustomCard(
     color: Color = Color(0xFFEADDFF),
     title: String,
     description: String,
+    icon: ImageVector
 ) {
     Card(
         modifier = modifier.fillMaxSize(),
@@ -104,6 +115,7 @@ fun CustomCard(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Icon(imageVector = icon, contentDescription = null)
             Text(
                 modifier = Modifier.padding(8.dp), text = title, fontWeight = FontWeight.Bold
             )
